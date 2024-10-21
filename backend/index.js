01ffb5207b50eea20224a7e5c8bb7d3d0b37f2d4
Alok-2004 <alokprasad02004@gmail.com>
2024-10-22 03:27:52 +0530

    POST request completed

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,32 @@
-const express = require("express")
+const  information  = require("./db");
+const infoCheck = require("./format");
+const express = require("express");
 const app = express();
 
+app.use(express.json());
 
+app.post("/add", (req, res) => {
+    const createPayLoad = req.body;
+    const check = infoCheck.safeParse(createPayLoad);
+    if (!check.success) {
+        return res.status(411).json({
+            message: "Format is Wrong"
+        });
+    }
 
-app.listen(3000 , ()=>{
-    console.log("Server is being run of PORT 3000");
-})
+    information.create(createPayLoad)
+        .then(() => {
+            res.status(201).json({ message: "Business card created successfully!" });
+        })
+        .catch((err) => {
+            res.status(500).json({ message: "Error creating business card", error: err });
+        });
+});
+
+app.get("" , (req , res) =>{
+
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on PORT 3000");
+});

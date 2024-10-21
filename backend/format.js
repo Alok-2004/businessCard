788fe52801c750a1e05f5672d84994707ce6bb61
Alok-2004 <alokprasad02004@gmail.com>
2024-10-22 02:40:09 +0530

    Done with schema for DB

---
+++ backend/format.js
@@ -0,0 +1,24 @@
+/*
+information = {
+    name ,
+    Description,
+    Interests = {
+        list of interests
+    }
+    
+    linkedin , Twitter
+}
+*/
+
+const zod = require("zod")
+
+const info = zod.object({
+    name : zod.string().min(1, " Name cant be Empty"),
+    description : zod.string()(1 , "Description Cant be Empty"),
+    interest : zod.array(zod.string()).nonempty(1 , "Atleast add one Interests"),
+    linkedin : zod.string().url("Invalid linedIn url"),
+    twitter : zod.string().url("Invalid X url")
+})
+
+// module.exports ={ info : info} or when the name of attribute is same  and only one entity
+module.exports = info

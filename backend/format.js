f03cc2f77991f93dd9ae47576e40009b533dcb1d
Alok-2004 <alokprasad02004@gmail.com>
2024-10-22 03:28:27 +0530

    Other

--- backend/format.js
+++ backend/format.js
@@ -12,13 +12,13 @@ information = {
 
 const zod = require("zod")
 
-const info = zod.object({
-    name : zod.string().min(1, " Name cant be Empty"),
-    description : zod.string()(1 , "Description Cant be Empty"),
+const infoCheck = zod.object({
+    name : zod.string().min(1, "Name cant be Empty"),
+    description : zod.string().min(1 , "Description Cant be Empty"),
     interest : zod.array(zod.string()).nonempty(1 , "Atleast add one Interests"),
     linkedin : zod.string().url("Invalid linedIn url"),
     twitter : zod.string().url("Invalid X url")
 })
 
 // module.exports ={ info : info} or when the name of attribute is same  and only one entity
-module.exports = info
+module.exports = infoCheck
